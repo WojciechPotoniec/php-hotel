53f637fa1e332059f5834badccf1bcf0df465922
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-02 20:11:06 +0200

    add filter on vote

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
 include __DIR__ . "/Models/data.php";
-var_dump($hotels);
 
 
-if (!empty($_GET['status']) || (isset($_GET['status']) && $_GET['status'] == 0)) {
-  $status = $_GET['status'];
-  $newArray = array_filter($hotels, function ($hotel) use ($status) {
-    return $hotel['parking'] == $status || $status == 'all';
+if (!empty($_GET['park']) || (isset($_GET['park']) && $_GET['park'] == 0) && !empty($_GET['vote']) || (isset($_GET['vote']) && $_GET['vote'] == 0)) {
+  $park = $_GET['park'];
+  $vote = $_GET['vote'];
+  $filt_park = array_filter($hotels, function ($hotel) use ($park) {
+    return $hotel['parking'] == $park || $park == 'all';
   });
+    $filt_vote = array_filter($filt_park, function ($hotel) use ($vote) {
+      return $hotel['vote'] >= $vote || $vote == 'all';
+    });
 } else {
-  $newArray = $hotels;
+  $filt_park = $hotels;
+  $filt_vote = $hotels;
 }
 
 
