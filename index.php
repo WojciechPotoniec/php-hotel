340bf36ae387038d586097979059bcfba3a32877
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-02 15:36:49 +0200

    init index

--- index.php
+++ index.php
@@ -2,14 +2,23 @@
 include __DIR__ . "/Models/data.php";
 var_dump($hotels);
 
+if (!empty($_GET['status']) || (isset($_GET['status']) && $_GET['status'] == 0)) {
+  $status = $_GET['status'];
+  $newArray = array_filter($hotels, function ($hotel) use ($status) {
+    return $hotel['parking'] == $status;
+  });
+} else {
+  $newArray = $hotels;
+}
 
 
 
-
-
+include __DIR__ . "/Views/header.php";
 ?>
 <main class="container">
- 
+  <?php
+  include __DIR__ . "/Views/table.php";
+  ?>
 </main>
 <?php
 include __DIR__ . "/Views/footer.php"
