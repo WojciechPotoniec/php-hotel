e4f0f2132ad3691a42f8d82e39779f679dd574a2
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-03 17:56:18 +0200

    update index

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
+
 <?php
+session_start();
+if (!isset($_SESSION['userId'])) {
+  session_destroy();
+  header('location: login.php');
+  die();
+}
+
 include __DIR__ . "/Models/data.php";
 
 
@@ -8,9 +16,9 @@ if (!empty($_GET['park']) || (isset($_GET['park']) && $_GET['park'] == 0) && !em
   $filt_park = array_filter($hotels, function ($hotel) use ($park) {
     return $hotel['parking'] == $park || $park == 'all';
   });
-    $filt_vote = array_filter($filt_park, function ($hotel) use ($vote) {
-      return $hotel['vote'] >= $vote || $vote == 'all';
-    });
+  $filt_vote = array_filter($filt_park, function ($hotel) use ($vote) {
+    return $hotel['vote'] >= $vote || $vote == 'all';
+  });
 } else {
   $filt_park = $hotels;
   $filt_vote = $hotels;
@@ -20,10 +28,16 @@ if (!empty($_GET['park']) || (isset($_GET['park']) && $_GET['park'] == 0) && !em
 
 include __DIR__ . "/Views/header.php";
 ?>
-<main class="container">
+<main class="container my-4">
+  <div class="d-flex justify-content-center align-items-center">
+    <h3><?= "Welcome " . $_SESSION['name'] ?></h3>
+  </div>
   <?php
   include __DIR__ . "/Views/table.php";
   ?>
+  <div class="d-flex justify-content-end">
+    <a href="logout.php" class="btn btn-danger">Logout</a>
+  </div>
 </main>
 <?php
 include __DIR__ . "/Views/footer.php"
