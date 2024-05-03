a939cdf4283d19c6df40cce92da5fcd56bc6a3eb
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-03 17:58:44 +0200

    created users array

---
+++ Models/user.php
@@ -0,0 +1,32 @@
+<?php
+
+$users = [
+    [
+        'id' => 1,
+        'name' => 'Nicola',
+        'surname' => 'Filannino',
+        'password' => '12345',
+        'email' => 'nicola@filannino.com',
+    ],
+    [
+        'id' => 2,
+        'name' => 'Luca',
+        'surname' => 'Sasso',
+        'password' => '12345',
+        'email' => 'luca@sasso.com',
+    ],
+    [
+        'id' => 3,
+        'name' => 'Samuele',
+        'surname' => 'Scavello',
+        'password' => '12345',
+        'email' => 'samuele@scavello.com',
+    ],
+    [
+        'id' => 4,
+        'name' => 'Angelo',
+        'surname' => 'Ciulla',
+        'password' => '11111',
+        'email' => 'angelo@ciulla.com',
+    ]
+];
